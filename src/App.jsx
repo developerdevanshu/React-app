63116c577d82fba1323916212d4832c22b56612e
developerdevanshu <devanshuagarwal164@gmail.com>
2025-04-09 17:07:02 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <div><h1>Hii this is devanshu AGARWAL</h1>
+          <h2> Welcome on my page</h2></div>
+  )
+}
+export default App
